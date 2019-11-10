60a702e459f1695951c49c0c1f5cf0a70b22f2e5
okozar <okozar@softserveinc.com>
2019-11-10 20:17:32 +0200

    Add authorisation

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,6 +1,69 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
 function Login() {
+
+	const history = useHistory();
+
+	const [loginData, setLoginData] = useState({
+		login: '',
+		password: ''
+	});
+
+	const handleInputChange = event => {
+		const { value, name } = event.target;
+		setLoginData({
+			...loginData,
+		  [name]: value
+		});
+	}
+
+	const onSubmit = event => {
+		event.preventDefault();
+		console.log(loginData);
+		fetch('http://localhost:3000/admin/login', {
+			method: 'POST',
+			body: JSON.stringify(loginData),
+			headers: {
+			  'Content-Type': 'application/json'
+			}
+		  })
+		  .then(res => {
+			  console.log('res', res)
+			if (res.status === 200) {
+			  history.push('/secret');
+			} else {
+			  const error = new Error(res.error);
+			  throw error;
+			}
+		  })
+		  .catch(err => {
+			console.error(err);
+			alert('Error logging in please try again');
+		  });
+	}
+
 	return (
-		"Login page"
+		<form onSubmit={onSubmit}>
+			<h1>Login!</h1>
+			<input
+			type="text"
+			name="login"
+			placeholder="Enter login"
+			value={loginData.login}
+			onChange={handleInputChange}
+			required
+			/>
+			<input
+			type="password"
+			name="password"
+			placeholder="Enter password"
+			value={loginData.password}
+			onChange={handleInputChange}
+			required
+			/>
+		<input type="submit" value="Submit"/>
+      </form>
 	);
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Login from './pages/Login/Login';
+import Secret from './pages/Secret/Secret';
 
 export default props => (
 	<Router>
 		<Switch>
 			<Route exact path="/login" component={Login} />
+			<Route exact path="/secret" component={Secret} />
 		</Switch>
 	</Router>
 );
